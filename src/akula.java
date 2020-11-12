9c041d5e23496031c3c37eff9e78926bdd4be314
javadjabbarli16 <javadjabbarli16@yahoo.com>
2020-11-12 10:45:55 -0500

    new

--- src/akula.java
+++ src/akula.java
@@ -1,7 +1,8 @@
-public class main {
+public class akula {
     public static void main(String[] args) {
         System.out.println("Hello Git last day");
 
         //b21
+        System.out.println("1234");
     }
 }
